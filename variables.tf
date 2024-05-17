6573dfdf62de3ba26ba9e4b39d5c5c8a19c1cb8e
Ninad.26 <ninad04d@gmail.com>
2024-05-18 00:27:55 +0530

    variables

--- variables.tf
+++ variables.tf
@@ -1,5 +1,9 @@
-variable "s3_bucket_name" { }
+variable "s3_bucket_name" {
+  description = "The name of the existing S3 bucket to store flow logs"
+  type        = string
+}
 
 variable "traffic_type" {
-  
+  description = "The type of traffic to capture. Valid values: ACCEPT, REJECT, ALL"
+  type        = string
 }

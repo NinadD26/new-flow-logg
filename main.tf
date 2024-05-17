686c6f68ca61d1d13f0132a5828cba549920a675
Ninad.26 <ninad04d@gmail.com>
2024-05-18 00:55:28 +0530

    vari

--- main.tf
+++ main.tf
@@ -5,8 +5,8 @@ provider "aws" {
 
 
 module "vpc_flow_log" {
-    source = "./modules/flowlog"
-    traffic_type = var.traffic_type
-    s3_bucket_name = var.s3_bucket_name
- 
+  source         = "./modules/flowlog"
+  traffic_type   = var.traffic_type
+  s3_bucket_name = var.s3_bucket_name
+
 }
--- variables.tf
+++ variables.tf
@@ -1,10 +1,11 @@
 variable "s3_bucket_name" {
   description = "The name of the existing S3 bucket to store flow logs"
   type        = string
-  default = "flowlog-bcktts"
+  default     = "flowlog-bcktts"
 }
 
 variable "traffic_type" {
   description = "The type of traffic to capture. Valid values: ACCEPT, REJECT, ALL"
   type        = string
+  default     = "ALL"
 }

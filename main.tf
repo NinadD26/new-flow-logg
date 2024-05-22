9c1e711a8c9ef9e308eef8d373f0bc6ba426db1c
Ninad.26 <ninad04d@gmail.com>
2024-05-22 18:14:23 +0530

    changes in mainnnnnnnn.tfffffffff

--- main.tf
+++ main.tf
@@ -1,16 +1,17 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = ">= 3.0.0"
-    }
-  }
-}
+# terraform {
+#   required_providers {
+#     aws = {
+#       source  = "hashicorp/aws"
+#       version = ">= 3.0.0"
+#     }
+#   }
+# }
 
 
 provider "aws" {
   region  = "us-east-1"
-  profile = "SandboxTeamA"
+  # profile = "SandboxTeamA"
+
 }
 
 
